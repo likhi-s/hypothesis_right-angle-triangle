e567f7fbbc6b311034476a7738769b4d94f2614f
Likitha <likithasshivareddy@gmail.com>
2024-10-16 12:46:37 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    double a,b,c;
+    cout<<"Enter the value of a: "<<endl;
+    cin>>a;
+    cout<<"Enter the value of b:"<<endl;
+    cin>>b;
+    c=sqrt((a*a)+(b*b));
+    cout<<"Hypothesis of right angle triangle is:"<<c<<endl;
+    return 0;
+}
